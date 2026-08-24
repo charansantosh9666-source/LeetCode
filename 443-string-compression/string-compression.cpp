d8a3a59af1db22346f52648e1e6985177a4e63c3
Charan santosh <25Q91A05AU@mrce.in>
2026-08-24 19:31:41 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (48.98%) - LeetSync

---
+++ 443-string-compression/string-compression.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int compress(vector<char>& chars) {
+        int idx=0;
+        int n=chars.size();
+        for(int i=0;i<n;i++){
+            char ch=chars[i];
+            int count=0;
+            while(i<n && ch==chars[i]){
+                count++,i++;
+            }
+            if(count==1){
+                chars[idx++]=ch;
+            }else{
+                chars[idx++]=ch;
+                string st=to_string(count);
+                for(char d:st){
+                    chars[idx++]=d;
+                }
+            }
+            i--;
+        }
+        chars.resize(idx);
+        return idx;
+    }
+};
